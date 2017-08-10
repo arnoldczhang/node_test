0d9e5e3c8c4e4599d4c239c56967306a660d7de1
arnoldzhang <arnold.c.zhang@gmail.com>
2017-08-10 22:49:48 +0800

    1.learn co;

--- app.js
+++ app.js
@@ -3,31 +3,62 @@ const utility = require('utility');
 const superagent = require('superagent');
 const cheerio = require('cheerio');
 const app = express();
+const co = require('co');
 
-app.get('/', function (req, res) {
-  superagent.get('https://cnodejs.org/')
-    .end(function (err, sres) {
-      // 常规的错误处理
-      if (err) {
-        return next(err);
-      }
-      // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
-      // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
-      // 剩下就都是 jquery 的内容了
-      var $ = cheerio.load(sres.text);
-      var items = [];
-      $('#topic_list .topic_title').each(function (idx, element) {
-        var $element = $(element);
-        items.push({
-          title: $element.attr('title'),
-          href: $element.attr('href')
-        });
-      });
+const say = () => {
+  return new Promise((res, rej) => {
+    console.log('say');
+  }).then((res) => {
+    return 'hello';
+  });
+};
 
-      res.send(items);
-    });
+const hello = (hello) => {
+  return new Promise((res, rej) => {
+    console.log('hello');
+    return 'world';
+  });
+};
+
+const world = () => {
+  return new Promise((res, rej) => {
+    console.log('world');
+  });
+};
+
+co(function* () {
+  yield say();
+  yield hello();
+  yield world();
 });
 
-app.listen(3000, function (req, res) {
-  console.log('app is running at port 3000');
-});
+
+
+
+// app.get('/', function (req, res) {
+//   superagent.get('https://cnodejs.org/')
+//     .end(function (err, sres) {
+//       // 常规的错误处理
+//       if (err) {
+//         return next(err);
+//       }
+//       // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
+//       // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
+//       // 剩下就都是 jquery 的内容了
+//       var $ = cheerio.load(sres.text);
+//       var items = [];
+//       $('#topic_list .topic_title').each(function (idx, element) {
+//         var $element = $(element);
+//         items.push({
+//           title: $element.attr('title'),
+//           href: $element.attr('href')
+//         });
+//       });
+
+//       res.send(items);
+//     });
+// });
+
+// app.listen(3000, function (req, res) {
+//   console.log('app is running at port 3000');
+// });
