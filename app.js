702c43fcacae2e52a572c6a5217ade321fceb8e5
arnold <zhangcheng17@meituan.com>
2017-09-04 18:15:44 +0800

    1.尝试解读anyproxy；

--- app.js
+++ app.js
@@ -1,42 +1,118 @@
 const express = require('express');
 const utility = require('utility');
 const http = require('http');
+const https = require('https');
+const HttpsProxyAgent = require('https-proxy-agent');
+const httpProxy = require('http-proxy');
 const superagent = require('superagent');
+const EasyCert = require('node-easy-cert');
 const cheerio = require('cheerio');
-// const app = express();
 const co = require('co');
 const koa = require('koa');
+const url= require("url");
+const fs = require('fs');
+const tls = require('tls');
 
+// const proxy = httpProxy.createProxyServer({
+//   target: {
+//     host: '127.0.0.1',
+//     port: 8080
+//   }
+// });
 
+// proxy.on('error', function (err, req, res) {  
+//   res.writeHead(500, {  
+//     'Content-Type': 'text/plain'  
+//   });  
+//   res.end('Something went wrong. And we are reporting a custom error message.');  
+// });
 
+const options = {
+  // rootDirPath: util.getUserHome() + '/.anyproxy_certs',
+  defaultCertAttrs: [
+    { name: 'countryName', value: 'CN' },
+    { name: 'organizationName', value: 'test_proxy' },
+    { shortName: 'ST', value: 'SH' },
+    { shortName: 'OU', value: 'test_proxy SSL Proxy' }
+  ]
+};
 
-class Nginx {
-  constructor(props) {
+const createServer = (key, cert) => {
+  const server = https.createServer({
+    key: fs.readFileSync(key) || fs.readFileSync('key.pem'),
+    cert: fs.readFileSync(cert) || fs.readFileSync('certificate.pem'),
+  }, (req, res) => {
+    console.log('Proxying https request at %s', new Date());
+    res.writeHead(200);
+    res.end('hello world\n');
+  });
+
+  server.listen(8000, () => {
+    console.log('server bound');
+  });
+};
+
+const easyCert = new EasyCert(options);
+const rootOptions = {
+  commonName: 'test_proxy',
+  overwrite: true,
+};
+
+easyCert.generateRootCA(rootOptions, (err, key, cert) => {
+  if (!err) {
+    console.log(key, cert);
+    createServer(key, cert);
   }
+});
 
-  callback() {
+// else {
+//   easyCert.getCertificate('127.0.0.1', (err, key, cert) => {
+//     if (!err) {
+//       createServer(key, cert);
+//     }
 
-    return (req, res) => {
+//     else {
+//       console.log(err);
+//     }
+//   });
+// }
 
-    };
-  }
 
-  use(fn) {
-    if (typeof fn === 'function') {
-      this.middleware.push(fn);
-    }
-  }
+// https.createServer(options.https, function(req, res) {
+//   console.log('Proxying https request at %s', new Date());
+//   proxy.proxyRequest(req, res);
+// }).listen(8888, function(err) {
+//   if (err)
+//     console.log('Error serving https proxy request: %s', req);
 
-  listen(...args) {
-    this.server = http.createServer(this.callback());
-    this.server.listen(...args);
-  }
-}
+//   console.log('Created https proxy. Forwarding requests from %s to %s:%s', '443');
+// });
 
-// const server = new Nginx();
 
-// server.listen(3001, () => {
 
+
+
+
+
+// const server = http.createServer(function(req, res) {
+//   const host = req.headers.host;
+//   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  
+//   switch(host){
+//     case 'takeaway.51ping.com':
+//       proxy.web(req, res, { target: `${host}:80` });
+//     default:
+//       res.writeHead(200, {
+//           'Content-Type': 'text/plain'
+//       });
+//       res.end('Welcome to my server!');
+//   }
+//   console.log('host=====>' + host);
+//   console.log('ip=====>' + ip);
+// });
+
+// server.listen(8888, () => {
+//   console.log('8888 start');
 // });
 
 
@@ -48,21 +124,23 @@ class Nginx {
 
 
 
-const app = new koa();
 
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  ctx.body = 'a a a';
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-}).use((ctx, next) => {
-  // ctx.body = 'bbbbbbbbbbb';
-  // ctx.response.body = 'bbaa';
-  console.log(ctx.response.body === ctx.body);
-}).listen(3000, () => {
-  console.log('koa app start at port 3000');
-});
+
+
+
+// const app = new koa();
+
+// app.use(async (ctx, next) => {
+//   const start = Date.now();
+//   ctx.body = 'a a a';
+//   await next();
+//   const ms = Date.now() - start;
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+// }).use((ctx, next) => {
+//   console.log(ctx.response.body === ctx.body);
+// }).listen(3000, () => {
+//   console.log('koa app start at port 3000');
+// });
 
 
 
